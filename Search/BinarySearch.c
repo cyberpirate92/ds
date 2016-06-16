e898646d4b5cd830e8d5e0cadecfed84e5391329
cyberpirate92 <bsravi.tez@gmail.com>
2016-06-16 15:40:44 +0530

    Added binary search (non-recursive) implementation

---
+++ Search/BinarySearch.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <malloc.h>
+
+// function prototypes
+int doBinarySearch(int *, int, int);
+void printArray(int *, int);
+void sort(int *, int);
+
+int main() {
+
+    int *a, i, size;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    a = (int *) malloc(sizeof(int) * size);
+
+    for(i=0; i<size; i++) {
+        printf("Element %d: ", (i+1));
+        scanf("%d", (a + i));
+    }
+
+    // sort the array
+    sort(a, size);
+    printf("Array after sort \n");
+    printArray(a, size);
+
+    do {
+        int data;
+        printf("Number to search: ");
+        scanf("%d", &data);
+        int index = doBinarySearch(a, size, data);
+        if(index == -1) {
+            printf("%d not found.\n", data);
+        }
+        else {
+            printf("%d found at position %d. \n", data, index);
+        }
+    }while(1);
+}
+
+int doBinarySearch(int *array, int arrLength, int searchData) {
+    int left, right, mid;
+    left = 0;
+    right = arrLength - 1;
+    mid = (left + right) / 2;
+
+    while(left <= right ) {
+        if(searchData == array[mid]) {
+            return mid;
+        }
+        else if(searchData > array[mid]) {
+            left = mid;
+        }
+        else {
+            right = mid;
+        }
+
+        mid = (left + right) / 2;
+
+        if(left == mid) {
+            left = mid + 1;
+            mid = (left + right) / 2;
+        }
+    }
+
+    return -1;
+}
+
+void printArray(int *array, int arrLength) {
+    int i;
+    for(i=0; i<arrLength; i++) {
+        printf("%d\t", *(array + i));
+    }
+    printf("\n");
+}
+
+void sort(int *array, int arrLength) {
+    int i, j;
+    for(i=0; i<arrLength; i++) {
+        for(j=i+1; j<arrLength; j++) {
+            if(array[j] < array[i]) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
