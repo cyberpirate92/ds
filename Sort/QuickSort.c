4fa0364d0f0da7af6fb3813a5414ff84fdd00bae
cyberpirate92 <bsravi.tez@gmail.com>
2016-06-18 19:30:33 +0530

    Finished Quick Sort Implementation

--- Sort/QuickSort.c
+++ Sort/QuickSort.c
@@ -1,82 +1,59 @@
 #include <stdio.h>
 #include <malloc.h>
+#define TRUE 1
+#define FALSE 0
 
 // function prototypes
 void printArray(int *, int);
 int quickSort(int *, int, int);
-void sort(int *, int);
+void sort(int *, int, int);
 void swap(int *, int *);
 
 int main() {
     int *a, i, size;
-
     printf("Enter size of array: ");
     scanf("%d", &size);
     a = (int *) malloc(sizeof(int) * size);
-
     for(i=0; i<size; i++) {
         printf("Element %d: ", (i+1));
         scanf("%d", (a + i));
     }
-
     printf("Initial array \n");
-    printArray(a, size);
-
-    sort(a, size);
-
+    printArray(a, size-1);
+    sort(a, 0, size-1);
     return 0;
 }
 
-void sort(int *array, int arrLength) {
+void sort(int *array, int left, int right) {
+    if(left < right) {
+        int splitIndex = quickSort(array, left, right);
+        printf("Array after first pivot \n");
+        printArray(array, right);
 
-    int splitIndex = quickSort(array, 0, arrLength-1);
-    printf("Array after first pivot \n");
-    printArray(array, arrLength);
+        // quicksort the left side (values < pivot value)
+        sort(array, left, splitIndex-1);
 
-    // quicksort the left side (values < pivot value)
-    quickSort(array, 0, splitIndex-1);
-    printf("Array after left half sort \n");
-    printArray(array, arrLength);
-
-    // quicksort the right side (values > pivot value)
-    quickSort(array, splitIndex+1, arrLength-1);
-    printf("Array after right half sort \n");
-    printArray(array, arrLength);
+        // quicksort the right side (values > pivot value)
+        sort(array, splitIndex+1, right);
+    }
 }
 
-int quickSort(int *array, int left, int right) {
-
-    int temp, pivot = left;
-    left += 1;
-
-    while( left <= right ) {
-        if(array[left] > array[pivot]) {
-            if(array[right] < array[pivot]) {
-                printf(" swapping %d and %d indexes\n", left, right);
-                swap(array + left, array + right);
-                left++;
-                right--;
-                if(left == right) {
-                    break;
-                }
-            }
-            else {
-                right--;
-            }
-        }
-        else
+int quickSort(int *array, int pivot, int right) {
+    int left = pivot +1;
+    int done = FALSE;
+    while( !done ) {
+        while( left < right && array[left] < array[pivot] ) {
             left++;
-        if(left >= right) {
-            swap(array+pivot, array+right);
-            break;
         }
+        while( right >= left && array[right] > array[pivot] ) {
+            right--;
+        }
+        if(right <= left)
+            done = TRUE;
+        else
+            swap(array +left, array + right);
     }
-    /*if(right > pivot)
-    {
-        printf(" swapping %d and %d indexes\n", pivot, right);
-        swap(array + pivot, array + right);
-        return right;
-    }*/
+    swap(array + right, array + pivot);
     return right;
 }
 
@@ -89,7 +66,7 @@ void swap(int *a, int *b) {
 
 void printArray(int *array, int arrLength) {
     int i;
-    for(i=0; i<arrLength; i++) {
+    for(i=0; i<=arrLength; i++) {
         printf("%d\t", *(array + i));
     }
     printf("\n");
